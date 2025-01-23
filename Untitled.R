e5eeb918d419bfb86c6ee6292609f32613f4729b
mariaabogdanska <maria.a.bogdanska@gmail.com>
2025-01-23 13:54:09 +0100

    new

--- Untitled.R
+++ Untitled.R
@@ -1,3 +1,14 @@
 ggygygygyhghbkvjhv
 hgiuygyfvfv
 kok
+bhcyxckj,k.nbhjgdc
+\khjvcxnfgxmdf,nmd
+jbhvhvjbv m
+
+
+
+install.packages("gitcreds")
+gitcreds::gitcreds_set()
+gitcreds::gitcreds_get()
+usethis::edit_r_environ()
+usethis::use_github(protocol = 'https')
